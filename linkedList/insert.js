81a3734ed2af5f34ec99ac117b832717f3f4d92b
ahmed thousif <ahmedthousif571@gmail.com>
2024-01-06 17:07:18 +0530

    linkedlist insert

---
+++ linkedList/insert.js
@@ -0,0 +1,88 @@
+//insert
+class Node {
+    constructor(values) {
+      this.value = values;
+      this.next = null;
+    }
+  }
+  class linkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
+    }
+    isEmpty() {
+      return this.size === 0;
+    }
+    getSize() {
+      return this.size;
+    }
+    prepend(values) {
+      const node = new Node(values);
+      if (this.isEmpty()) {
+        this.head = node;
+      } else {
+        node.next = this.head;
+        this.head = node;
+      }
+      this.size++
+    }
+  
+    append(values) {
+      const node = new Node(values);
+      if (this.isEmpty()) {
+        this.head = node;
+      } else {
+        let prev = this.head;
+        while (prev.next) {
+          prev = prev.next;
+        }
+        prev.next = node;
+      }
+      this.size++;
+    }
+    insert(values, index) {
+      if (index < 0 || index > this.size) {
+        return;
+      }
+      if (index === 0) {
+        this.prepend(values);
+      } else {
+        const node = new Node(values);
+        let prev = this.head;
+        for (let i = 0; i < index - 1; i++) {
+          prev = prev.next;
+        }
+        node.next = prev.next;
+        prev.next = node;
+        this.size++;
+      }
+    }
+    print() {
+      if (this.isEmpty()) {
+        console.log("list is empty");
+      } else {
+        let curr = this.head;
+        let listValues = "";
+        while (curr) {
+          listValues += `${curr.value}`;
+          curr = curr.next;
+        }
+        console.log(listValues);
+      }
+    }
+  }
+  
+  const list = new linkedList();
+  console.log("list is empty", list.isEmpty());
+  console.log("get size", list.getSize());
+  
+  list.print();
+  
+  list.insert(10, 0);
+  list.print();
+  
+  list.insert(20, 0);
+  list.print();
+  
+  list.insert(40, 1);
+  list.print();
--- practise/practise.js
+++ practise/practise.js
@@ -16,60 +16,129 @@
 // }
 // console.log(factRec(5));
 
-
 //adding a node in beginning
-class Node{
-    constructor(value){
-        this.value = value
-        this.next = null
-    }
-}
-class linkedList{
-    constructor(){
-        this.head = null
-        this.size= 0
-    }
-    isEmpty(){
-        return this.size ===0
-    }
-    getSize(){
-        return this.size
-    }
-    prepend(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = node
-        }else{
-            node.next = this.head
-            this.head = node
-        }
-        this.size++
-    }
-    print(){
-        if(this.isEmpty()){
-            console.log('empty');
-        }else{
-            let curr = this.head
-            let listValues = ''
-            while(curr){
-                listValues += `${curr.value} `
-                curr = curr.next
-            }
-            console.log(listValues);
-        }
-        
-    }
-}
-const list = new linkedList()
-console.log("list is empty", list.isEmpty());
-console.log("get size ", list.getSize());
-list.print();
+// class Node{
+//     constructor(value){
+//         this.value = value
+//         this.next = null
+//     }
+// }
+// class linkedList{
+//     constructor(){
+//         this.head = null
+//         this.size= 0
+//     }
+//     isEmpty(){
+//         return this.size ===0
+//     }
+//     getSize(){
+//         return this.size
+//     }
+//     append(value){
+//         const node = new Node(value)
+//         if(this.isEmpty()){
+//             this.head = node
+//         }else{
+//             node.next = this.head
+//             this.head = node
+//         }
+//         this.size++
+//     }
+//     print(){
+//         if(this.isEmpty()){
+//             console.log('empty');
+//         }else{
+//             let curr = this.head
+//             let listValues = ''
+//             while(curr){
+//                 listValues += `${curr.value} `
+//                 curr = curr.next
+//             }
+//             console.log(listValues);
+//         }
 
-list.prepend(10);
-list.print();
+//     }
+// }
+// const list = new linkedList()
+// console.log("list is empty", list.isEmpty());
+// console.log("get size ", list.getSize());
+// list.print();
 
-list.prepend(20);
-list.prepend(30);
-list.print();
+// list.append(10);
+// list.print();
+
+// list.append(20);
+// list.append(30);
+// list.print();
+
+//day 3
+// class Node {
+//     constructor(values){
+//         this.value = values
+//         this.next = null
+//     }
+// }
+// class linkedList{
+//     constructor(){
+//         this.head = null
+//         this.size= 0
+//     }
+//     isEmpty(){
+//         return this.size===0
+//     }
+//     getSize(){
+//         return this.size
+//     }
+//     append(values){
+//         const node = new Node(values)
+//         if(this.isEmpty()){
+//             this.head = node
+//         }else{
+//             node.next = this.head
+//             this.head = node
+//         }
+//         this.size++
+//     }
+//     append(values){
+//         const node = new Node(values)
+//         if(this.isEmpty()){
+//             this.head = node
+//         }else{
+//             let prev = this.head
+//             while(prev.next){
+//                 prev = prev.next
+//             }
+//             prev.next = node
+//         }
+//         this.size++
+//     }
+
+//     print(){
+//         if(this.isEmpty()){
+//             console.log('list is empty');
+//         }else{
+//             let curr = this.head
+//             let listValues=''
+//             while(curr){
+//                 listValues += `${curr.value} `
+//                 curr = curr.next
+//             }
+//             console.log(listValues);
+//         }
+//     }
+// }
+
+// const list = new linkedList()
+// console.log('list is empty',list.isEmpty());
+// console.log('get size',list.getSize());
+
+// list.print()
+
+// list.append(10)
+// list.print()
+
+// list.append(20)
+// list.append(30)
+// list.print()
 
 
