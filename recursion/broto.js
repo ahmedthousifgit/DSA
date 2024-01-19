e22e108b315f7355cfab88c8cd24073ad084a9a8
ahmed thousif <ahmedthousif571@gmail.com>
2024-01-19 21:12:28 +0530

    broto

---
+++ recursion/broto.js
@@ -0,0 +1,9 @@
+function f(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  f(n - 1);
+  console.log(n);
+  f(n - 1);
+}
+console.log(f(5));
--- recursion/nthCase.js
+++ recursion/nthCase.js
@@ -1,7 +1,7 @@
-//n th num in fibonacci sequence
+
 function recursivefib(n) {
   if (n < 2) {
-    //base case for terminating recursion
+  
     return n;
   }
   return recursivefib(n - 1) + recursivefib(n - 2);
