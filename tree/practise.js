5ae27fde70c411f207d4fbd6e1367e8987544742
ahmed thousif <ahmedthousif571@gmail.com>
2024-02-05 23:47:42 +0530

    practise done

--- tree/practise.js
+++ tree/practise.js
@@ -1,3 +1,4 @@
+// validate bst
 class Node {
   constructor(value) {
     this.value = value;
@@ -39,52 +40,93 @@ class binarySearchTree {
   search(root, value) {
     if (!root) {
       return false;
+    }
+    if (root.value === value) {
+      return true;
+    } else if (value < root.value) {
+      return this.search(root.left, value);
     } else {
-      if (root.value === value) {
-        return true;
-      } else if (value < root.value) {
-        return this.search(root.left, value);
-      } else {
-        return this.search(root.right, value);
-      }
-    }
-  }
-  preOrder(root) {
-    if (root) {
-      console.log(root.value);
-      this.preOrder(root.left);
-      this.preOrder(root.right);
+      return this.search(root.right, value);
     }
   }
 
-  inOrder(root) {
-    if (root) {
-      this.preOrder(root.left);
-      console.log(root.value);
-      this.preOrder(root.right);
+  // isBst() {
+  //   return this.isValid(this.root, -Infinity, Infinity);
+  // }
+  // isValid(node, min, max) {
+  //   if (!node) {
+  //     return true;
+  //   }
+  //   if (node.value <= min || node.value >= max) {
+  //     return false;
+  //   }
+  //   return (
+  //     this.isValid(node.left, min, node.value) &&
+  //     this.isValid(node.right, node.value, max)
+  //   );
+  // }
+
+  isBST(node, min, max) {
+    if (!node) {
+      return true;
+    }
+    if (node.value < min || node.value > max) {
+      return false;
+    }
+    return (
+      this.isBST(node.left, min, node.value) &&
+      this.isBST(node.right, node.value, max)
+    );
+  }
+  min(root) {
+    if (!root.left) {
+      return root.value;
+    } else {
+      return this.min(root.left);
     }
   }
 
-  postOrder(root) {
-    if (root) {
-      this.preOrder(root.left);
-      this.preOrder(root.right);
-      console.log(root.value);
+  max(root) {
+    if (!root.right) {
+      return root.value;
+    } else {
+      return this.max(root.right);
     }
   }
 
-  levelOrder() {
-    let que = [];
-    que.push(this.root);
-    while (que.length) {
-      let curr = que.shift();
-      console.log(curr.value);
-      if (curr.left) {
-        que.push(curr.left);
+  delete(value) {
+    return this.deleteNode(this.root, value);
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      } else if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
       }
-      if (curr.right) {
-        que.push(curr.right);
-      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
+
+  height(root) {
+    if (!root) {
+      return 0;
+    } else {
+      const leftHeight = this.height(root.left);
+      const rightHeight = this.height(root.right);
+      return Math.max(leftHeight, rightHeight) + 1;
     }
   }
 }
@@ -95,7 +137,10 @@ tree.insert(5);
 tree.insert(15);
 tree.insert(3);
 tree.insert(7);
+tree.insert(12);
 
-console.log(tree.search(tree.root, 34));
-// tree.inOrder(tree.root);
-tree.levelOrder();
+// tree.delete(tree.root, 10);
+// console.log(tree.search(tree.root, 10));
+// console.log(tree.isBST());
+// console.log(tree.max(tree.root));
+console.log(tree.height(tree.root));
---
+++ tree/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(4);
